78c86240a871fdefe97143fb30bc0c19aec4b0d4
Furkan Salman <salmanfurkan28@gmail.com>
2023-04-10 14:22:48 +0200

    Final state of gomoku.py

--- lab2/gomoku.py
+++ lab2/gomoku.py
@@ -11,12 +11,12 @@ board = [[' ' for _ in range(board_size)] for _ in range(board_size)]
 def print_board():
     print("   ", end="")
     for i in range(board_size):
-        print(chr(ord('a') + i), end=" ")
+        print(chr(ord('a') + i), end="  ")
     print()
     for i in range(board_size):
         print("{:2d}".format(i + 1), end=" ")
         for j in range(board_size):
-            print(board[i][j], end=" ")
+            print(board[i][j], end="  ")
         print()
 
 
